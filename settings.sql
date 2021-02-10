623cfaab0d91b05b68de3ab07c7aab9c4891b87e
abduljames <abduljames@Abduls-iMac.local>
2021-02-09 23:17:54 -0500

    got postgres working

--- settings.sql
+++ settings.sql
@@ -2,9 +2,12 @@ CREATE DATABASE child_care;
 CREATE USER child_careuser
 WITH PASSWORD 'password';
 ALTER ROLE child_careuser
-SET client_encoding TO 'utf8';
+SET client_encoding
+TO 'utf8';
 ALTER ROLE child_careuser
-SET default_transaction_isolation TO 'read committed';
+SET default_transaction_isolation
+TO 'read committed';
 ALTER ROLE child_careuser
-SET timezone TO 'UTC';
+SET timezone
+TO 'UTC';
 GRANT ALL PRIVILEGES ON DATABASE child_care TO child_careuser;
