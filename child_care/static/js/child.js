82e23a7afe696b3dd3df7d7fe74a2c1aefe87a95
abduljames <abduljames@Abduls-iMac.local>
2021-02-15 20:55:37 -0500

    menu button

--- child_care/static/js/child.js
+++ child_care/static/js/child.js
@@ -1,4 +1,9 @@
-function myFunction(x){
-    x.classList.toggle('change')
+function myFunction(){
+    var x = document.getElementById("links")
+    if (x.style.display == "block") {
+        x.style.display == "none";
+    } else {
+        x.style.display = "x"
+    }
 
 }
